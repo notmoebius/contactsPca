9586f6f572d1c01680ef349bc1066bf5e3ee18eb
notmoebius <notmoebius@gmail.com>
2019-09-22 22:26:30 +0200

    add json to file export

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+# trouver dans : https://stackoverflow.com/questions/31927229/how-to-add-json-file-export-functionality-in-rails-4-2
+def contacts_json
+    data = Contact.all.to_json
+    send_data data, :type => 'application/json; header=present', :disposition => "attachment; filename=annauire.json"
+end
 end
