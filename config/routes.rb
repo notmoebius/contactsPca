7f3e5695fa0b0e3f7fdd70b5956dad23a6be3aa8
notmoebius <notmoebius@gmail.com>
2019-09-22 18:29:32 +0200

    fix html

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,14 @@ Rails.application.routes.draw do
   resources :contacts do
     collection { post :import }
   end
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   controller :pages do
     #get :home
+    post :import
     get :export
     get :about
     get :memo
   end
 end
+
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
